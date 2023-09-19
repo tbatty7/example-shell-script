e857679a7133f2c39e76e044b0cb3c322524cb04
Tim Batty <tbatty7@gmail.com>
2023-09-18 20:44:16 -0400

    check for bashrc

--- hello.sh
+++ hello.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # this is a comment
 echo Hello World    # This is also a comment
-echo the files in the folder are:
+echo the files in this folder are:
 ls
 echo My shell is: $SHELL
 basename $(readlink /proc/$$/exe)
+echo My version of Vim is: 
+vim --version | grep Vi
+echo Do I have a .bashrc file?
+ls -a ~/ | grep bashrc
