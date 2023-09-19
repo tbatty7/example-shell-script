ef6f99389ba47f2d242c94b9b2a775402515d25e
Tim Batty <tbatty7@gmail.com>
2023-09-18 21:15:33 -0400

    added memory, git, pwd and environment variables

--- hello.sh
+++ hello.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # this is a comment
 echo Hello World    # This is also a comment
+echo My current working directory is:
+pwd
 echo the files in this folder are:
 ls
+echo The version of Git I have installed:
+git --version
 echo My shell is: $SHELL
 basename $(readlink /proc/$$/exe)
 echo My version of Vim is: 
@@ -13,3 +17,7 @@ echo My operating system is:
 uname -a
 echo My path variable is:
 echo $PATH
+echo My memory is:
+free || vm_stat || systeminfo | grep Memory
+echo My environment variables are:
+env
